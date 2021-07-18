b876dcd300521db3b2e2ad43149019db4a05bc3d
Kamihara <yoshihara72@gmail.com>
2021-07-18 17:19:33 +0900

    add array.ts

---
+++ src/array.ts
@@ -0,0 +1,18 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [4, 5, 6];
+
+let strings2: Array<string> = ['Tokyo', 'Osaka', 'Fukuoka'];
+
+let strings: string[] = ['JavaScript', 'TypeScript', 'CoffeeScript'];
+
+// let twoDimArray: Array<Array<number>> = [
+let twoDimArray: number[][] = [
+    [10, 20],
+    [30, 50],
+];
+
+let hairetsu: (number|boolean|string)[] = [1, false, 'status'];
+// let hairetsu: (number|boolean|string)[] = [1, false, 'status', [1, 2, 3]];
