c35a9a2ca6565aac0a539e858070b4b29980daa3
Kamihara <yoshihara72@gmail.com>
2021-07-18 16:59:50 +0900

    add number-string.ts

---
+++ src/number-string.ts
@@ -0,0 +1,13 @@
+export {};
+
+let year: number = 1989;
+// year = false
+console.log(year);
+
+let age: number = 0x20;
+
+console.log(age)
+
+let name: string = 'ykami'
+// name = 10
+console.log(name)
